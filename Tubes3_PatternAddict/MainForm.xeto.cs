8af6e59b25d6ca2f4f9851cb7691c251b5d0a0f0
atpur-rafif <muhammadatpurrafif@gmail.com>
2024-05-13 14:00:30 +0700

    Open file dialog

--- Tubes3_PatternAddict/MainForm.xeto.cs
+++ Tubes3_PatternAddict/MainForm.xeto.cs
@@ -6,53 +6,62 @@ delegate void Runnable();
 
 namespace Tubes3_PatternAddict
 {
-	public class MainForm : Form
-	{
-		public MainForm()
-		{
-			Resizable = false;
-			Content = new TableLayout
-			{
-				Spacing = new Size(5, 5),
-				Padding = new Padding(10, 10, 10, 10),
-				Rows =
-				{
-					new TableRow(
-						new Label
-						{
-							Text = "Aplikasi C# Tugas Besar 3 Strategi Algoritma 2023/2024",
-							TextAlignment = TextAlignment.Center
-						}
-					),
-					new TableRow(
-						new ImageView { Height = 300 },
-						new ImageView { Height = 300 },
-						new ImageView { Height = 300 }
-					),
-					new TableRow(
-						new Button
-						{
-							Text = "Pilih Citra",
-							Command = new Command(chooseFileDialog)
-						},
-						new Button { Text = "BM/KMP" },
-						new Button { Text = "Search" },
-						new TableLayout
-						{
-							Rows =
-							{
-								new TableRow(new Label { Text = "Waktu Pencarian: 0s" }),
-								new TableRow(new Label { Text = "Presentase Kecocokan: 0%" })
-							},
-						}
-					)
-				}
-			};
-		}
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            EventHandler<EventArgs> chooseFileDialog = (object o, EventArgs e) =>
+                    {
+                        String[] extensions = new String[] { "png", "jpg", "bmp" };
+                        var filter = new FileFilter("image", extensions);
 
-		EventHandler<EventArgs> chooseFileDialog = (object o, EventArgs e) =>
-		{
-			Console.WriteLine("File dialog clicked!");
-		};
-	}
+                        var dialog = new OpenFileDialog();
+                        dialog.Filters.Add(filter);
+                        dialog.CurrentFilterIndex = 0;
+                        dialog.ShowDialog(this);
+
+                        Console.WriteLine(dialog.FileName);
+                    };
+
+            Resizable = false;
+            Content = new TableLayout
+            {
+                Spacing = new Size(5, 5),
+                Padding = new Padding(10, 10, 10, 10),
+                Rows =
+                {
+                    new TableRow(
+                        new Label
+                        {
+                            Text = "Aplikasi C# Tugas Besar 3 Strategi Algoritma 2023/2024",
+                            TextAlignment = TextAlignment.Center
+                        }
+                    ),
+                    new TableRow(
+                        new ImageView { Height = 300 },
+                        new ImageView { Height = 300 },
+                        new ImageView { Height = 300 }
+                    ),
+                    new TableRow(
+                        new Button
+                        {
+                            Text = "Pilih Citra",
+                            Command = new Command(chooseFileDialog)
+                        },
+                        new Button { Text = "BM/KMP" },
+                        new Button { Text = "Search" },
+                        new TableLayout
+                        {
+                            Rows =
+                            {
+                                new TableRow(new Label { Text = "Waktu Pencarian: 0s" }),
+                                new TableRow(new Label { Text = "Presentase Kecocokan: 0%" })
+                            },
+                        }
+                    )
+                }
+            };
+        }
+
+    }
 }
