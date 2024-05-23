203f5fdef2cea0fc8e1926eeba47d611a7b16393
Intermaze <rafikiprawhira@gmail.com>
2024-05-23 13:50:46 +0700

    feat: convert to Capitalized Case, added more alay cases

--- Algorithm/Regex.cs
+++ Algorithm/Regex.cs
@@ -4,12 +4,19 @@ using System.Text.RegularExpressions;
 class RegularExpression
 {
 	private (string, string)[] alayToNormalMap = new (string, string)[]{
-            (@"4", "a"),
-            (@"3", "e"),
-            (@"1", "i"),
+            // Dalam spesifikasi, bahasa alay yang non-karakter berupa angka
+            // Ada non-karakter alay lain selain angka, tetapi tidak dihandle disini (silahkan ditambahkan jika perlu)
+            (@"13", "b"),
+            (@"17", "d"),
             (@"0", "o"),
+            (@"1", "i"), //bisa 1/l
+            (@"2", "z"),
+            (@"3", "e"),
+            (@"4", "a"),
             (@"5", "s"),
-            (@"7", "t"),
+            (@"6", "g"), //bisa 6/b
+            (@"7", "j"),
+            (@"9", "g"), 
         };
 
     public string ConvertAlayToNormal(string alayText)
@@ -17,8 +24,15 @@ class RegularExpression
         string result = alayText;
         foreach (var (bahasaAlay, bahasaNormal) in alayToNormalMap)
         {
-            result = Regex.Replace(result, bahasaAlay, bahasaNormal, RegexOptions.IgnoreCase);
+            result = Regex.Replace(result, bahasaAlay, bahasaNormal);
         }
+
+        // Konversi menjadi Capitalized Case
+        // Konversi semua huruf besar menjadi kecilnya
+        result = Regex.Replace(result, @"[A-Z]", (Match match) => match.ToString().ToLower());
+
+        // Konversi semua huruf di awal kata menjadi huruf besar
+        result = Regex.Replace(result, @"\b\w", (Match match)=> match.ToString().ToUpper());
 
         return result;
     }
