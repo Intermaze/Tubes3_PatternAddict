fc7615c977a3a2d5968bc45f9c7ddff206218bc1
Intermaze <rafikiprawhira@gmail.com>
2024-05-23 15:37:27 +0700

    feat: TODO regex compare with database

--- Algorithm/Regex.cs
+++ Algorithm/Regex.cs
@@ -36,4 +36,31 @@ class RegularExpression
 
         return result;
     }
+
+    // TODO: test dan masukkin ke main
+    // Fungsi untuk konversi bahasa alay ke normal, lalu lakukan Algo.FindClosestMatch untuk mencari nama yang paling dekat di database
+
+    // public List<(string, string, int)> CompareAlayWithDatabase(string alay, string[] database){
+    //     List<(string, string, int)> result = new List<(string, string, int)>();
+    //     string pattern = ConvertAlayToNormal(alay);
+
+    //     foreach (var data in database){
+    //         if (pattern.Equals(data)){
+    //             result.Add((pattern, data, 0));
+    //         }
+    //         else{
+    //             (string, int) closestMatch = FindClosestMatch(pattern, data);
+    //             if (!string.IsNullOrEmpty(closestMatch.Item1)){
+    //                 int distanceEachChar = Algo.CalculateCharDifference(pattern, closestMatch.Item1);
+    //                 result.Add((closestMatch.Item1, data, distanceEachChar + closestMatch.Item2));
+    //             }
+    //             else{
+    //                 Console.WriteLine($"{data} is not found. {pattern} is too long to compare");
+    //             }
+    //         }
+    //     }
+
+    //     result = result.OrderBy(tuple => tuple.Item3).ToList();
+    //     return result;
+    // }
 }
